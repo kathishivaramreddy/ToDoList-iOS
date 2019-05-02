87e9043f444ba4ad265a3420d104e39ba4403de7
ssh <kathishivaram@gmail.com>
2019-05-02 10:55:54 +0530

    add tableview controller and navigation controller

---
+++ TODO-List/TodoTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TodoTableViewController.swift
+//  TODO-List
+//
+//  Created by apple on 5/2/19.
+//  Copyright © 2019 shivaapple. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        return cell
+    }
+
+}
