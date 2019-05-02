2a372214259db488dbdabee0ff155c1da0353722
ssh <kathishivaram@gmail.com>
2019-05-02 14:17:00 +0530

    add ui to todolist add viewcontroller

---
+++ TODO-List/ToDoAddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoAddViewController.swift
+//  TODO-List
+//
+//  Created by apple on 5/2/19.
+//  Copyright © 2019 shivaapple. All rights reserved.
+//
+
+import UIKit
+
+class ToDoAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
